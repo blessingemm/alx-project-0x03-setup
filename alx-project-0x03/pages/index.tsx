1a2fb8d4ed49474768836320a2fff68e13c3bd91
Blessing <eblessing793577@gmail.com>
2025-06-07 16:53:32 +0100

    corrected the set up for imperative routing

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -6,11 +6,11 @@ interface PageRouteProps {
 }
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   // Imeperative routing with useRouter
   const routeToNextPage  = ({ pageRoute }: PageRouteProps) => {
-    router.push(pageRoute, undefined, { shallow: false})
+    router.push(pageRoute, undefined, { shallow: false});
   }
 
   return (
